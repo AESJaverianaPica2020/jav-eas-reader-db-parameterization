2eb8d62ad35ecbef4fb2c288ab52b6f6e0777a4f
ingAndresMartinez <57421413+ingAndresMartinez@users.noreply.github.com>
2020-10-04 13:44:31 -0500

    Add files via upload

---
+++ src/main/java/co/edu/javeriana/eas/pica/toures/balon/JavEasReaderDbParameterizationApplication.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.eas.pica.toures.balon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavEasReaderDbParameterizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavEasReaderDbParameterizationApplication.class, args);
+	}
+
+}
